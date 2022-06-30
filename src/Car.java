7d8fa1ec415c9b76eb1c9f48d182a4b2934e0463
ozgurtechno <ozgur.technostudy@gmail.com>
2022-06-30 12:26:38 +0200

    gitignore added

--- src/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car {
-    private String name;
-    private String model;
-
-    public Car(String name, String model) {
-        this.name = name;
-        this.model = model;
-    }
-}
