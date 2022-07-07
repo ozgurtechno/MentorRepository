e374d6239087b8ff7cc9b712813e2ef622d33dea
Ozgur Techno <ozgur.technostudy@gmail.com>
2022-07-07 16:33:47 +0200

    Polymorphism

--- src/polymorphism/Parent.java
+++ src/polymorphism/Parent.java
@@ -13,11 +13,19 @@ class Child1 extends Parent {
     void methodOfSuperClass() {
         System.out.println("From Child1 Class");
     }
+
+    void methodOfChild1Class(){
+        System.out.println("This method belongs to Child Class");
+    }
 }
 
 class Child2 extends Parent {
     //Super Class Method Overrided
     void methodOfSuperClass() {
         System.out.println("From Child2 Class");
+    }
+
+    void methodOfChild2Class(){
+        System.out.println("This method belongs to Child Class");
     }
 }
--- src/polymorphism/UtilityClass.java
+++ src/polymorphism/UtilityClass.java
@@ -13,7 +13,24 @@ public class UtilityClass {
         Child1 child1 = new Child1();
         Child2 child2 = new Child2();
 
+        var weDontKnowType = new Parent(); // Diyelim ki bu obje program esnasinda duruma gore degisebiliyor...
 
+        // Without Polymorphism
+        if (weDontKnowType instanceof Parent){
+
+            parent.methodOfSuperClass();
+
+        } else if (weDontKnowType instanceof Child1) {
+
+            child1.methodOfChild1Class();
+
+        } else if (weDontKnowType instanceof Child2) {
+
+            child2.methodOfChild2Class();
+
+        }
+
+        // With Polymorphism
         parent.methodOfSuperClass();
         child1.methodOfSuperClass();
         child2.methodOfSuperClass();
@@ -21,7 +38,7 @@ public class UtilityClass {
         System.out.println("----------------------------");
 
         util(parent);  //SuperClass object is passes to util()
-        util(child1);     //SubClass object is passed to util()
-        util(child2);    //SubClass2 object is passed to util()
+        util(child1);  //SubClass object is passed to util()
+        util(child2);  //SubClass2 object is passed to util()
     }
 }
